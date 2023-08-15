bcf13ce3c27157c998e8a152373cab36a26a84a8
KamilArici <kamilarici5@gmail.com>
2023-08-15 23:54:03 +0300

    edit

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,12 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-import stockReducer from "../features/stockSlice"
+import stockReducer from "../features/stockSlice";
+
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
 const store = configureStore({
   reducer: {
-    auth: authReducer,
-    stock:stockReducer
-
+    auth: persistedReducer,
+    stock: stockReducer,
   },
   // devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store);
 export default store;
